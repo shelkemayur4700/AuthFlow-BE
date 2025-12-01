4e08fdd1e010e07a038fb3cd1caa155464f8697e
mayur <shelkemayur4700@gmail.com>
2025-12-01 18:18:02 +0530

    [REFACT]:added route to test db connection

--- src/app.js
+++ src/app.js
@@ -4,13 +4,20 @@ const authRoutes = require("./app/routes/auth.routes");
 const app = express();
 const cors = require("cors");
 const { errorMiddleware } = require("./app/middleware");
+const connectDB = require("./app/utils/db");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
+app.get("/test-db", async (req, res) => {
+  try {
+    await connectDB();
+    res.status(200).json({ message: "DB Connected!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.use("/auth", authRoutes);
 app.use("/user", userRouter);
