24824e03f124fb8ee8bd208315d561b1a6e1f869
mayur <shelkemayur4700@gmail.com>
2025-12-01 18:05:57 +0530

    [REFACT]: mongoose timeout issue resolved

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose");
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  console.log("database connected sucessfully!");
-  await mongoose.connect(process.env.DATABASE_URL);
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, 
+    };
+
+    cached.promise = mongoose
+      .connect(process.env.DATABASE_URL, opts)
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("Database connected successfully!");
+  return cached.conn;
 };
 
 module.exports = connectDB;
